aab0cd8805aca4bacef99daf2c93b5392ae61eee
Anastasia <D:\Mail>
2023-06-02 13:44:46 +0300

    .

--- bot_api/api/views.py
+++ bot_api/api/views.py
@@ -1,10 +1,16 @@
-from rest_framework.views import APIView
+import os
+
 import requests
+from dotenv import load_dotenv
+from rest_framework.views import APIView
+from telegram import Telegram
 
 from api.serializers import MessageSerializer
-from bot_api.main import BOT_TOKEN
 
-SEND_MESSAGE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/messages.sendMessage'
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+SEND_MESSAGE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
 
 
 class AskBotView(APIView):
@@ -15,4 +21,18 @@ class AskBotView(APIView):
         )
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        response = requests.post(SEND_MESSAGE_URL, data=serializer.data)
+        tg = Telegram(
+            api_id=os.getenv('API_ID'),
+            api_hash=os.getenv('API_HASH'),
+            phone=os.getenv('PHONE'),
+            database_encryption_key='changeme1234',
+        )
+        tg.login()
+        result = tg.get_chats()
+        result.wait()
+        result = tg.send_message(
+            chat_id=318345947,
+            text='dfnnvjfkdvn jkxs',
+        )
+        result.wait()
+        tg.stop()
